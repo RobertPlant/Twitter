1be62a5735ad8599c701a307aab39477d8a0efaf
Rob <trebotplant@gmail.com>
2013-10-18 11:11:02 +0100

    removed deleted files from git

--- webroot/signup.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-session_start();
-include 'autoload.php';
-
-$_SESSION['user']['current'] = new User($_POST['name']);
-$_SESSION['user'][] = new User($_POST['name']);
-
-header( 'Location: /' ) ;
