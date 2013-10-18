90b13fec4a22cb61d42a375d947c0e0e8213a3a6
mgill2190 <martin.gill@warrant-group.com>
2013-10-18 15:32:24 +0100

    registration page linked up

--- src/view/homepage.php
+++ src/view/homepage.php
@@ -6,7 +6,7 @@ Username: <input type="text" name="user_name"><br>
 Password: <input type="password" name="password"><br>
 <input type="submit" value="Log In">
 </form>
-Not registered? Click <a href="registration.php">here to register</a>.
+Not registered? Click <a href="register">here to register</a>.
 </body>
 </html>
 
