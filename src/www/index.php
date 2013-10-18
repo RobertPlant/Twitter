2714d03bd75f78c0e038e24872b4f9476cd5f06b
Rob <trebotplant@gmail.com>
2013-10-18 11:59:51 +0100

    added router

--- src/www/index.php
+++ src/www/index.php
@@ -2,8 +2,14 @@
 include '../autoload.php';
 session_start();
 
+if ($_SERVER['REDIRECT_URL'] != '/') 
+{
+	$route = $_SERVER['REDIRECT_URL'];
+} else {
+	$route = 'index';
+}
+$route .= 'Action';
 // check the URL for where to route to
-
 // if empty, go to default controller
 $controller = new DefaultController();
-$controller->indexAction();
+$controller->$route();
