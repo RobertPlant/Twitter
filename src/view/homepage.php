82ec3d529c8c7821b51c5466da04dc5310846b8f
mgill2190 <martin.gill@warrant-group.com>
2013-10-18 12:02:47 +0100

    added skeletens for views

---
+++ src/view/homepage.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+<form>
+<h1>TWITTER</h1>
+Username: <input type="text" name="user_name"><br>
+Password: <input type="password" name="password">
+<input type="submit" value="Log In">
+</form>
+</body>
+</html>
+
---
+++ src/view/registration.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+<form>
+<h1>TWITTER REGISTRATION</h1>
+Fill in below to register for twitter:
+Username: <input type="text" name="user_name"><br>
+Password: <input type="password" name="password"><br>
+First Name: <input type="text" name="first_name"><br>
+Last Name: <input type="text" name="last_name">
+<input type="submit" value="Log In">
+</form>
+</body>
+</html>
+
