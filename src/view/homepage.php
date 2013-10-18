a6fe65d1a46115ff1119bb77acfd874b0dc00709
mgill2190 <martin.gill@warrant-group.com>
2013-10-18 15:28:23 +0100

    Linking homepage

--- src/view/homepage.php
+++ src/view/homepage.php
@@ -6,9 +6,7 @@ Username: <input type="text" name="user_name"><br>
 Password: <input type="password" name="password"><br>
 <input type="submit" value="Log In">
 </form>
-<form action="registration.php">
-    <input type="submit" value="Register">
-</form>
+Not registered? Click <a href="registration.php">here to register</a>.
 </body>
 </html>
 
